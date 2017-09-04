cd215d9df829f96d014f788576d97946fd21428f
Sumit Singh <sumitsinghsakhwar@gmail.com>
2017-09-05 01:00:07 +0530

    LocalDB class added for local database queries. Hardcoded examples removed.

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -2,48 +2,47 @@ package application;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
-
-import java.util.Date;
+import java.sql.SQLException;
 
 public class MainController {
 
     @FXML
-    private ListView<People> list;
-    private ObservableList<People> olist;
-    private ObservableList<Message> mlist;
+    private ListView<People> peopleListView;
+    public ObservableList<People> peopleList;
+    public ObservableList<Message> messageList;
     @FXML
-    private ListView<Message> messagelist;
+    private ListView<Message> messageListView;
+    private String currentUser;
+    private LocalDB db;
 
     @FXML
     public void initialize() {
         System.out.println("Initialize");
-        olist= FXCollections.observableArrayList();
-        People sumit = new People("Sumit","satan_nigthmare","");
-        People anubhav = new People("Anubhav","satan_nigthmare","");
-        People shakti = new People("Shakti","satan_nigthmare","");
-        olist.addAll(sumit,anubhav,shakti);
-        list.setOnMouseClicked(mouseEvent -> {
+        db=new LocalDB(this);
+        peopleList= FXCollections.observableArrayList();
+        try {
+            db.setUsers();
+        } catch (SQLException e) {
+            System.out.println("SQL error in setting users");
+            e.printStackTrace();
+        }
+        peopleListView.setOnMouseClicked(mouseEvent -> {
             System.out.println("Button Clicked");
+            try {
+                db.setAllMessages(peopleListView.getSelectionModel().getSelectedItem());
+            } catch (SQLException e) {
+                System.out.println("Local Database Error");
+                e.printStackTrace();
+            }
             //temp.setText(list.getSelectionModel().getSelectedItem().name);
         });
-        list.setItems(olist);
-        mlist=FXCollections.observableArrayList();
-        String s="Hey there.Hey there.Hey there.Hey there.Hey there.Hey there.Hey there.Hey there.Hey there.Hey there.Hey there.";
-        mlist.addAll(
-                new Message("Hey there.",anubhav,sumit, new Date()),
-                new Message("How are you",anubhav,sumit, new Date()),
-                new Message("I don't know",sumit,anubhav, new Date()),
-                new Message("What's the problem",anubhav,sumit, new Date()),
-                new Message("You know what, fuck you",sumit,anubhav, new Date()),
-                new Message(s,anubhav,sumit,new Date())
-                );
-        messagelist.setItems(mlist);
-        messagelist.setCellFactory(messageListView -> new MessageListViewCell());
+        peopleListView.setItems(peopleList);
+        messageList=FXCollections.observableArrayList();
+        messageListView.setItems(messageList);
+        messageListView.setCellFactory(messageListView -> new MessageListViewCell());
     }
+
 
 }
