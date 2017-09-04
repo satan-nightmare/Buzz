d4c78c91034a1c13ade713489d1fc856641bc7f6
Garvit Gupta <garvitgupta58@gmail.com>
2017-09-04 09:30:33 -0700

    added ReceivingThread.java and SendingThread.java and removed UserThread.java

--- src/application/Server.java
+++ src/application/Server.java
@@ -2,21 +2,31 @@ package application;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Server {
 
     public static void  main(String args[]) throws Exception{
 
         ServerSocket serverSocket = new ServerSocket(7777);
-
+        Connection con = getDatabaseConnection();
         while (true) {
             Socket clientSocket = serverSocket.accept();
-            UserThread user = new UserThread(clientSocket);
-            Thread userThread = new Thread(user);
-            //new thread for individual user started
-            userThread.start();
+            ReceivingThread receivingThread=new ReceivingThread(clientSocket,con);
+            SendingThread sendingThread=new SendingThread(clientSocket,con);
+            Thread receive = new Thread(receivingThread);
+            Thread send = new Thread(sendingThread);
+            receive.start();
+            send.start();
         }
 
 
+    }
+    private static Connection getDatabaseConnection() throws Exception{
+        Class.forName("java.sql.DriverManager");
+        Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/BuzzServer","root","");
+        return con;
     }
 }
